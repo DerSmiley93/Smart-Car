b4f08b77c41dda26b9e8e488356ab65176a958d7
DerSmiley93 <Philliphecker@hotmail.com>
2020-12-25 17:38:05 +0100

    fixed Tacho

--- www/main.js
+++ www/main.js
@@ -13,21 +13,33 @@ setInterval(() => {
     } else {
         time.tabIndex = time.innerHTML = "0" + date.getHours() + ":" + "0" + date.getMinutes()
     }
-},1)
+}, 1)
 getLocation()
 function getLocation() {
     if (navigator.geolocation) {
 
+        setInterval(() => {
+            navigator.geolocation.getCurrentPosition(pos)
+            navigator.geolocation.getCurrentPosition(pos)
+            navigator.geolocation.getCurrentPosition(pos)
+        },500)
+        
 
-        //navigator.geolocation.getCurrentPosition(pos)
-        navigator.geolocation.watchPosition(pos)
 
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function pos(Cpos) {
-    console.log(Cpos.speed)
-    Tacho.innerHTML = Math.floor(Cpos.coords.speed * 3.6)
+var speeds = new Array();
+
+var i = 0;
+function pos(location) {
+    
+    speeds[i] = location.coords.speed;
+    i++;
+    if(i == 3){
+        i = 0;
+        Tacho.innerHTML = ((speeds[0] + speeds[1] + speeds[2]) / 3) * 3.6;
+    }
 }
